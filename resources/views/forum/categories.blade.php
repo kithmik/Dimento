dd2737eeec14214ed13c5b033119e62a11bd1cc4
dhanikabulath <dhanikabulathsinhala@gmail.com>
2017-12-06 17:40:12 +0530

    updated

--- resources/views/forum/categories.blade.php
+++ resources/views/forum/categories.blade.php
@@ -1,12 +1,245 @@
-<!DOCTYPE html>
+<!DOCTYPE <html>
+    <head>
+        <title>Dimento Forum</title>
 
-<html>
-<head>
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Dimento Forum</title>
-</head>
-<body>
+        <!-- Font Awesome -->
+    <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
+
+    <!-- Bootstrap core CSS -->
+    <link href="/libs/mdb4/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Material Design Bootstrap -->
+    <link href="/libs/mdb4/css/mdb.css" rel="stylesheet">
+
+    <!-- Material Design Bootstrap Extra-->
+    <link href="/libs/mdb4/css/compiled.min.css" rel="stylesheet">
+
+        <!-- JQuery -->
+    <script type="text/javascript" src="/libs/mdb4/js/jquery-3.1.1.min.js"></script>
+
+    <style type="text/css">
+        body{
+            padding-top: 65px;
+        }
+    </style>
+
+    </head>
+    <body>
+
+        @include('includes.navbar')
 
 
-</body>
+
+<br />
+<br />
+
+<!-- <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="ca">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card hoverable" style="width:300px">
+                        <div class="text-center">
+                           <i class="fa fa-graduation-cap fa-5x card-img-top" aria-hidden="true"></i>
+                               <h4 class="card-title">Education</h4>
+                               <p class="card-text">Find 3D models of Education </p>
+                               <div class="card-footer">
+                               <a href="#" class="btn btn-primary">Dive in</a>
+                           </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card hoverable" style="width:300px">
+                        <div class="text-center">
+                           <i class="fa fa-car fa-5x aria-hidden="true"></i>
+
+                               <h4 class="card-title">Vehicles</h4>
+                               <p class="card-text">Find 3D models of Vehicles </p>
+                               <div class="card-footer">
+                               <a href="#" class="btn btn-primary">Dive in</a>
+                           </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                     <div class="card hoverable" style="width:300px">
+                        <div class="text-center">
+                            <i class="fa fa-wheelchair-alt fa-5x" aria-hidden="true"></i>
+
+                               <h4 class="card-title">Health</h4>
+                               <p class="card-text">Find 3D models of Health </p>
+                               <div class="card-footer">
+                               <a href="#" class="btn btn-primary">Dive in</a>
+                           </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <br>
+            <div class="row">
+                <div class="col-md-4">
+                <div class="card hoverable" style="width:300px">
+                        <div class="text-center">
+                            <i class="fa fa-file-image-o fa-5x" aria-hidden="true"></i>
+
+                            <h4 class="card-title">Art</h4>
+                            <p class="card-text">Find 3D models of Art </p>
+                            <div class="card-footer">
+                            <a href="#" class="btn btn-primary">Dive in</a>
+                           </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card hoverable" style="width:300px">
+                        <div class="text-center">
+                            <i class="fa fa-gamepad fa-5x" aria-hidden="true"></i>
+
+                               <h4 class="card-title">Gaming</h4>
+                               <p class="card-text">Find 3D models of Gaming </p>
+                               <div class="card-footer">
+                               <a href="#" class="btn btn-primary">Dive in</a>
+                           </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                     <div class="card hoverable" style="width:300px">
+                        <div class="text-center">
+                            <i class="fa fa-user-circle-o fa-5x" aria-hidden="true"></i>
+
+                               <h4 class="card-title">Characters</h4>
+                               <p class="card-text">Find 3D models of Characters</p>
+                               <div class="card-footer">
+                               <a href="#" class="btn btn-primary">Dive in</a>
+                           </div>
+                        </div>
+                    </div>
+                </div>
+
+
+        </div>
+
+                    <br>
+            <div class="row">
+                    <div class="col-md-4">
+                    <div class="card hoverable" style="width:300px">
+                            <div class="text-center">
+                                        <i class="fa fa-music fa-5x" aria-hidden="true"></i>
+
+                                            <h4 class="card-title">Music</h4>
+                                            <p class="card-text">Find 3D models of Music </p>
+                                            <div class="card-footer">
+                                            <a href="#" class="btn btn-primary">Dive in</a>
+                                       </div>
+                                    </div>
+                                </div>
+                            </div>
+
+    
+                <div class="col-md-4">
+                            <div class="card hoverable" style="width:300px">
+                            <div class="text-center">
+                                <i class="fa fa-tree fa-5x" aria-hidden="true"></i>
+
+                                <h4 class="card-title">Tree</h4>
+                                <p class="card-text">Find 3D models of Tree </p>
+                                <div class="card-footer">
+                                <a href="#" class="btn btn-primary">Dive in</a>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+                <div>
+                   <div class="col-md-4">
+                            <div class="card hoverable" style="width:300px">
+                            <div class="text-center">
+                                <i class="fa fa-wifi fa-5x" aria-hidden="true"></i>
+
+
+                                <h4 class="card-title">Technology</h4>
+                                <p class="card-text">Find 3D models of Technology </p>
+                                <div class="card-footer">
+                                <a href="#" class="btn btn-primary">Dive in</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>       
+                </div>                
+        </div>
+
+            <br>
+            <div>
+                <div class="row">
+                    <div class="col-md-4">
+                    <div class="card hoverable" style="width:300px">
+                            <div class="text-center">
+                                        <i class="fa fa-rocket fa-5x" aria-hidden="true"></i>
+
+
+                                        <h4 class="card-title">Travel</h4>
+                                        <p class="card-text">Find 3D models of Travel </p>
+                                        <div class="card-footer">
+                                        <a href="#" class="btn btn-primary">Dive in</a>
+                                       </div>
+                                    </div>
+                                </div>
+                            </div>
+                    <div>
+                        <div class="col-md-4">
+                    <div class="card hoverable" style="width:300px">
+                            <div class="text-center">
+                                        <i class="fa fa-futbol-o fa-5x" aria-hidden="true"></i>
+
+
+
+                                        <h4 class="card-title">Travel</h4>
+                                        <p class="card-text">Find 3D models of Travel </p>
+                                        <div class="card-footer">
+                                        <a href="#" class="btn btn-primary">Dive in</a>
+                                       </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+            </div>
+    </div>
+</div>
+
+                
+
+<!-- SCRIPTS -->
+
+<!-- Bootstrap dropdown -->
+<script type="text/javascript" src="/libs/mdb4/js/popper.min.js"></script>
+
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="/libs/mdb4/js/bootstrap.min.js"></script>
+
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="/libs/mdb4/js/mdb.js"></script>
+
+<script type="text/javascript" src="/libs/mdb4/js/compiled.min.js"></script>
+
+
+    </body>
 </html>
