59dbcc18d4546f45b1431570b1cd2390977d7a41
Anjeyster <anjeyster@gmail.com>
2017-12-07 08:39:12 +0530

    updated

--- app/Http/Controllers/Object/ObjectController.php
+++ app/Http/Controllers/Object/ObjectController.php
@@ -11,7 +11,7 @@ class ObjectController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except(['index', 'show']);
     }
 
     /**
--- resources/views/objects/create.blade.php
+++ resources/views/objects/create.blade.php
@@ -12,14 +12,14 @@
     <div class="row">
         <div class="col-md-6 mx-auto">
 
-            <form  action="{{ route('object.create') }}" method="post" enctype="multipart/form-data" id="obj-create-form" name="obj-create-form">
+            <form  action="{{ route('object.store') }}" method="post" enctype="multipart/form-data" id="obj-create-form" name="obj-create-form">
                 {{ csrf_field() }}
                 <!--Rotating card-->
                 <div class="card-wrapper">
-                    <div id="card-1" class="card-rotating effect__click">
+                    <div id="card-1" class="card-rotating effect__click m-1 p-1 pb-2" style="display: inline-block;">
 
                         <!--Front Side-->
-                        <div class="face front">
+                        <div class="face front" id="face-front">
 
                             <!-- Image-->
                         {{--<div class="card-up">
@@ -32,13 +32,13 @@
                             <div class="card-body">
                                 <h4>Upload a new 3D model</h4>
 
-                                <div id="drop-obj">
+                                {{--<div id="drop-obj">
                                     <div class="dropzone-previews" style="display: inline-block;"></div>
                                     <div class="file-field dropzone" id="drop-image">
                                         <div class="fallback">
                                             <div class="btn">
                                                 <span>Objects</span>
-                                                <input type="file" id="object" name="object" {{--accept="image/*"--}}>
+                                                <input type="file" id="object" name="object" --}}{{--accept="image/*"--}}{{-->
                                             </div>
                                             <div class="file-path-wrapper">
                                                 <input class="file-path validate" type="text" placeholder="This will be the main object that appears on the wall">
@@ -47,7 +47,64 @@
                                         <div class="input-field"></div>
                                         <div id="fileDisplayArea"></div>
                                     </div>
+                                </div>--}}
+
+                                <div class="" id="file-input-div">
+                                    <div class="row">
+                                        <div class="card z-depth-0 hoverable mx-auto ">
+
+                                            <div class="row">
+                                                <div class="col-md-12 ">
+                                                    <div class="row">
+                                                        <div class="col-md-12">
+                                                            <div class="file-field">
+                                                                <div class="btn btn-elegant btn-sm">
+                                                                    <span>Choose A 3D object file</span>
+                                                                    <input type="file" name="object" id="object">
+                                                                </div>
+                                                                <div class="file-path-wrapper">
+                                                                    <input class="file-path validate" type="text" placeholder=".3ds, .obj, .md2">
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="row">
+                                                        <div class="col-md-12">
+                                                            <div class="file-field">
+                                                                <div class="btn btn-elegant btn-sm">
+                                                                    <span>Choose A Texture</span>
+                                                                    <input type="file" name="texture" id="texture">
+                                                                </div>
+                                                                <div class="file-path-wrapper">
+                                                                    <input class="file-path validate" type="text" placeholder=".jpg, .png">
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+
+                                        </div>
+                                    </div>
+
                                 </div>
+
+                                <br>
+                                <div class="" id="extra-files-div">
+
+                                </div>
+
+
+                                <div class="row">
+                                    <div class="col-md-12 pull-right mr-auto text-right">
+                                        <button type="button" id="add-more-btn" class="btn btn-outline-default waves-effect btn-sm">
+                                            Add More Objects
+                                        </button>
+                                    </div>
+                                </div>
+
 
                                 {{--<p>Web developer</p>--}}
                                 <!--Triggering button-->
@@ -61,10 +118,68 @@
 
                             <!--Content-->
                             {{--<h4>About me</h4>--}}
+                            <h4>Say something About this Model</h4>
                             <hr>
-                            <p>
+                            {{--<p>
 
-                            </p>
+                            </p>--}}
+
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="md-form">
+                                            {{--<i class="fa fa-text-width prefix"></i>--}}
+                                            <input type="text" id="title" name="title" class="form-control" length="150">
+                                            <label for="title">Title</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="md-form">
+                                            {{--<i class="fa fa-text-width prefix"></i>--}}
+
+                                            <select class="mdb-select" id="category" name="category">
+                                                <option value="" disabled selected>Select Category</option>
+                                                <option value="Animals">Animals</option>
+                                                <option value="Architecture">Architecture</option>
+                                                <option value="Art">Art</option>
+
+                                                <option value="Characters">Characters</option>
+                                                <option value="Food">Food</option>
+                                                <option value="Furniture">Furniture</option>
+                                            </select>
+                                            <label for="category">Select Category</label>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="md-form">
+                                            {{--<i class="fa fa-text-width prefix"></i>--}}
+                                            <textarea type="text" id="description" name="description" class="md-textarea" > </textarea>
+                                            <label for="description">Description</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="md-form">
+                                            <input type="submit" class="btn btn-outline-elegant btn-rounded" name="submit" id="submit">
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+
+
                             <hr>
                           {{--  <!--Social Icons-->
                             <ul class="inline-ul">
@@ -87,16 +202,92 @@
         </div>
     </div>
 
+    <script>
+        var i = 0;
+        $(function () {
+
+
+            $('.mdb-select').material_select();
+
+            //Appending new form elements when add more is clicked
+
+           $('#add-more-btn').click(function (e) {
+               e.preventDefault();
+               i++;
+
+               var card = $('#face-front');
+
+               var extra_ele_div = $('#extra-files-div');
+
+//               card.height(card.height()-extra_ele_div.height());
+
+               var beforeH = extra_ele_div.height();
+               console.log('before '+extra_ele_div.height());
+
+               var html_eles = '<div class="appended-div" data-id="'+i+'"> <div class="row"> <div class="card z-depth-1 hoverable"> <div class="card-body"> <div class="row"> <div class="col-md-10"> <div class="row"> <div class="col-md-12"> <div class="file-field"> <div class="btn btn-elegant btn-sm"> <span>Choose A 3D object file</span> <input type="file" name="objects[]" id="objects-'+i+'"> </div> <div class="file-path-wrapper"> <input class="file-path validate" type="text" placeholder="3D model object files"> </div> </div> </div> </div> <div class="row"> <div class="col-md-12"> <div class="file-field"> <div class="btn btn-elegant btn-sm"> <span>Choose A Texture</span> <input type="file" name="textures[]" id="texture-'+i+'"> </div> <div class="file-path-wrapper"> <input class="file-path validate" type="text" placeholder=".jpg, .png"> </div> </div> </div> </div> </div> <div class="col-md-2"> <div class=" text-right"> <button type="button" class="btn btn-flat close-btn" data-id="'+i+'"> <i class="fa fa-window-close"></i> </button> </div> </div> </div> </div> </div> </div> <br> </div>  ';
+
+               extra_ele_div.append(html_eles);
+
+
+               var afterH = extra_ele_div.height();
+               console.log('after '+extra_ele_div.height());
+
+               card.height(card.height()+(afterH - beforeH));
+
+           });
+
+           //Removing the added elements when close button is clicked
+
+            $(document).on('click', '.close-btn', function (e) {
+                e.preventDefault();
+
+                var id = $(this).attr('data-id');
+
+                var card = $('#face-front');
+
+                var extra_ele_div = $('#extra-files-div');
+
+//                card.height(card.height()-extra_ele_div.height());
+
+                var beforeH = extra_ele_div.height();
+                console.log('before '+extra_ele_div.height());
+
+                $('.appended-div[data-id="'+id+'"').remove();
+
+                var afterH = extra_ele_div.height();
+                console.log('after '+extra_ele_div.height());
+
+
+
+                card.height(card.height()-(beforeH - afterH));
+            });
+
+
+        });
+    </script>
 
     <script>
-        var myDropzone;
+
+/*        Dropzone.autoDiscover = false;
+
+        $('#drop-obj').dropzone({
+            url: "/file/post",
+            clickable: true
+        });
+
+        var myDropzone;*/
         /*var data = {};
         var post_id = -1;
         var i = 0;
         var isImage = false;*/
-        var my_dropzone = new Dropzone("div#drop-obj" , { url: "/file/post"});
+//        var my_dropzone = new Dropzone("div#drop-obj" , { url: "/file/post"});
+//        var my_dropzone = new Dropzone("div#drop-obj" , {
 //        myDropzone.opetio
-        Dropzone.options.dropImage = { // The camelized version of the ID of the form element
+//        Dropzone.options.dropImage = { // The camelized version of the ID of the form element
+/*
+
+my_dropzone.options.dropImage = { // The camelized version of the ID of the form element
+
 
             // The configuration we've talked about above
             dictDefaultMessage: "Drag and drop or click to upload your object files",
@@ -111,7 +302,7 @@
             filesizeBase: 1024,
             addRemoveLinks: true,
             clickable: true,
-//            acceptedFiles: 'image/*, .gif',
+//            acceptedFiles: 'image/!*, .gif',
             headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
             },
@@ -134,15 +325,15 @@
 
                 // First change the button to actually tell Dropzone to process the queue.
 //                this.element.querySelector("button[id='img_submit']").addEventListener("click", function(e) {
-                /*document.querySelector("button[id='submit']").addEventListener("click", function(e) {
+                /!*document.querySelector("button[id='submit']").addEventListener("click", function(e) {
                  // Make sure that the form isn't actually being sent.
                  e.preventDefault();
                  e.stopPropagation();
 
                  //myDropzone.processQueue();
-                 });*/
+                 });*!/
 
-                /*// Listen to the sendingmultiple event. In this case, it's the sendingmultiple event instead
+                /!*!// Listen to the sendingmultiple event. In this case, it's the sendingmultiple event instead
                  // of the sending event because uploadMultiple is set to true.
                  this.on("sendingmultiple", function() {
                  // Gets triggered when the form is actually being sent.
@@ -155,7 +346,7 @@
                  this.on("errormultiple", function(files, response) {
                  // Gets triggered when there was an error sending the files.
                  // Maybe show form again, and notify user of error
-                 });*/
+                 });*!/
                 myDropzone.on("sending", function(file, xhr, formData) {
 //
                 });
@@ -177,6 +368,7 @@
 
         };
 
+*/
 
 
     </script>
