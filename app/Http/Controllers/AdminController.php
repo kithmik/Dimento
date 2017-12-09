54e40f95870129bcfe9f2cfc277f9dc3c106800d
Anjeyster <anjeyster@gmail.com>
2017-12-09 21:22:28 +0530

    updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -28,7 +28,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        if (auth()->user()->isAdmin()){
+        if (auth()->user()->isAdmin() || 1){
             $users = User::all();
             $objects = Object::all();
             $posts = Post::all();
